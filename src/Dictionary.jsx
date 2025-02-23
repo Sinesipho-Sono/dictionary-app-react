4fa1cb4a65735f7d36ecc0d98f887e871c97611f
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-02-23 11:23:17 +0200

    added API integration

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
-export default function Dictionary(event) {
+export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiKey = "dfc8b6f54adt4d38bbe0o47364a63d82";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
