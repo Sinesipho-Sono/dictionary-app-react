31c2f389c341a416fad4a71570bb2f60ee7d7b01
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-02-23 19:24:14 +0200

    added synonyms component

--- src/Meaning.jsx
+++ src/Meaning.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>
-        1.
-        {props.meaning.definition}
-      </p>
+      <p>1. {props.meaning.definition}</p>
       {props.meaning.example && (
         <p>
-          <em>2. "{props.meaning.example}"</em>
+          <em>
+            2. <strong> Example:</strong> "{props.meaning.example}"
+          </em>
         </p>
       )}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
---
+++ src/Synonyms.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms && props.synonyms.length > 0) {
+    return (
+      <div className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
