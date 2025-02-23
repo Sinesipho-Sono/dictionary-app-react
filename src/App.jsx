8033532d27c45e6818e848c95a51bade107371e3
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-02-23 08:44:18 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App ">
+      <h1 className="text-center">Dictionary App</h1>
+      <footer className="text-center">
+        This website was coded by Sinesipho Sono and it is open-sourced
+      </footer>
+    </div>
+  );
+}
+
+export default App;
