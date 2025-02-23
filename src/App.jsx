45cff552ac5eb6ea18aa48d3b2691e0de91dc9e5
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-02-23 09:46:47 +0200

    added the search engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
-
+import Dictionary from "./Dictionary.jsx";
 function App() {
   return (
     <div className="App ">
-      <h1 className="text-center">Dictionary App</h1>
-      <footer className="text-center">
-        This website was coded by Sinesipho Sono and it is open-sourced
-      </footer>
+      <div className="container">
+        <h1 className="text-center">Dictionary App</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center m-5">
+          <small>
+            This website was coded by Sinesipho Sono and it is open-sourced
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
